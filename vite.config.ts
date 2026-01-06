c282dc2dc2428074b801f36dfe34c2ca32f4c57c
Crownnyuja <tlfltmtm@gmail.com>
2026-01-06 10:35:30 +0700

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    base: '/schedule/',
     return {
       server: {
         port: 3000,
