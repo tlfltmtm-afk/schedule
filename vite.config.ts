7520e26aeaf66c8ceff7b41eecd35b672e88c7c0
Crownnyuja <tlfltmtm@gmail.com>
2026-01-06 10:47:40 +0700

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    
     return {
-      base: '/schedule/', 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+      base: '/schedule/', // 빌드 경로 설정
+      server: { port: 3000, host: '0.0.0.0' },
       plugins: [react()],
       define: {
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+        alias: { '@': path.resolve(process.cwd(), '.') }
       }
     };
 });
